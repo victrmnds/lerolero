29e281bea626b1ad5b99522b1069cab58f7de819
Victor <victor.grandolfo@usp.br>
2025-03-20 10:53:50 -0300

    Insere as frases

--- lerolero.py
+++ lerolero.py
@@ -6,8 +6,8 @@ import random
 
 #Cada frase é composta por três partes aleatórias;
 
-parte1 = []
-parte2 = []
-parte3 = []
+parte1 = ["O sistema em desenvolvimento", "O novo protocolo de comunicação", "O algoritmo otimizado"]
+parte2 = ["possui excelente desempenho", "oferece garantias de precisão acima da média", "preenche uma lacuna significativa"]
+parte3 = ["nas aplicações a que se destina", "em relação às opções disponíveis no mercado", ", provendo ampla vantagem competitiva a seus usuários"]
 
 print (random.choice(parte1), random.choice(parte2), random.choice(parte3))
