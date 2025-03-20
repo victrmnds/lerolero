1af05723c553b18ae2642f87832018e549b95565
Victor <victor.grandolfo@usp.br>
2025-03-20 11:08:42 -0300

    Melhora um dos textos

--- lerolero.py
+++ lerolero.py
@@ -6,7 +6,7 @@ import random
 
 #Cada frase é composta por três partes aleatórias;
 
-parte1 = ["O sistema em desenvolvimento", "O novo protocolo de comunicação", "O algoritmo otimizado"]
+parte1 = ["O sistema em desenvolvimento", "O novo protocolo de comunicação", "O algoritmo de otimização"]
 parte2 = ["possui excelente desempenho", "oferece garantias de precisão acima da média", "preenche uma lacuna significativa"]
 parte3 = ["nas aplicações a que se destina", "em relação às opções disponíveis no mercado", ", provendo ampla vantagem competitiva a seus usuários"]
 
