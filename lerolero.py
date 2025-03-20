2922d54bf9153957154eb9d5c441486c8e723007
Sophia Mariano <sophiasoaresmariano@usp.br>
2025-03-20 10:44:42 -0300

    Cria esqueleto da versão de multilínguas.

--- lerolero.py
+++ lerolero.py
@@ -10,4 +10,11 @@ parte1 = []
 parte2 = []
 parte3 = []
 
+lingua = int(input("Escolha a língua: 1- português; 2- inglês\n"))
+
+if lingua == 2:
+	parte1 = []
+	parte2 = []
+	parte3 = []
+
 print (random.choice(parte1), random.choice(parte2), random.choice(parte3))
